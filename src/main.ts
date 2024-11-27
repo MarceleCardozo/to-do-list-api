e69295e80fe759c406d59e3c2c2392b06e8d1372
Marcele Cardozo <marcelecardozo@gmail.com>
2024-11-27 20:58:49 -0300

    start the project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(process.env.PORT || 8080);
+  const logger = new Logger('Application');
+  logger.log('App started at http://localhost:8080');
 }
+
 bootstrap();
